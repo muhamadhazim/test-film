aa2365ffd5ec11d44a6229d2f2fe5050915c011c
Muhammad Hazim Robbani <44898036+muhamadhazim@users.noreply.github.com>
2025-12-19 15:57:30 +0700

    Update client.ts

--- src/services/client.ts
+++ src/services/client.ts
@@ -4,8 +4,8 @@ const client = axios.create({
   baseURL: "https://ga-mobile-api.loklok.tv/cms/app",
   headers: {
     lang: "en",
-    versioncode: "11",
-    clienttype: "ios_jike_default",
+    versioncode: "12",
+    clienttype: "ios_default",
   },
 });
 
